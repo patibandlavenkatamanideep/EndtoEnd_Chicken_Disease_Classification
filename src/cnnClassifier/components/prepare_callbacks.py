456616981b138395fdb2bafc6d601642d3c2c4e7
Venkata Manideep <venkatamanideep20@gmail.com>
2025-12-23 03:37:14 -0500

    Prepare Callbacks added

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -10,6 +10,8 @@ class PrepareCallback:
     def __init__(self, config: PrepareCallbacksConfig):
         self.config = config
 
+
+
     @property
     def _create_tb_callbacks(self):
         timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
@@ -19,12 +21,15 @@ class PrepareCallback:
         )
         return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
 
+
     @property
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
-            filepath=self.config.checkpoint_model_filepath,
-            save_best_only=True
-        )
+        filepath=str(self.config.checkpoint_model_filepath),
+        save_best_only=True
+    )
+
+
 
     def get_tb_ckpt_callbacks(self):
         return [
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,24 +1,21 @@
-import os
-
 from cnnClassifier.constants import *
+import os
+from pathlib import Path
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig, PrepareCallbacksConfig
-                                                )
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig)
 
 
 class ConfigurationManager:
     def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
-
+            self,
+            config_filepath=CONFIG_FILE_PATH,
+            params_filepath=PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
-
-
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
