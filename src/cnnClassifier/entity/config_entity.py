c9fedd07c8370482c443c294a53d1a753d6e4b92
Venkata Manideep <venkatamanideep20@gmail.com>
2025-12-23 05:28:56 -0500

    dvc added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -29,7 +29,6 @@ class PrepareCallbacksConfig:
 
 
 
-
 @dataclass(frozen=True)
 class TrainingConfig:
     root_dir: Path
@@ -40,3 +39,12 @@ class TrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
